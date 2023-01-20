873da2a415028d1ec4f7153ddccd149b42691a3e
simanavets <ilyasimonovec@mail.ru>
2023-01-20 16:43:33 +0100

    project initialization

---
+++ src/test/java/com/simanavets/tenderflexbackend/TenderFlexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simanavets.tenderflexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenderFlexBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
